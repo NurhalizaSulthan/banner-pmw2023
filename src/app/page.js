b4326cf63b22388d7ee7d0e18086cb422744519d
NurhalizaSulthan <nurhalizas21d@student.unhas.ac.id>
2023-11-09 14:50:21 +0700

    feat: web profil

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,27 @@
 
+'use client'
+
+import { useAmp } from 'next/amp';
 import Image from 'next/image'
+import './globals.css'
+import { useState } from 'react';
 
 export default function Home() {
+
+  
+  const [bravo, setBravo] = useState(0)
+  const [nama, setNama] = useState('Siti Nurhaliza')
+
+  function handlerTambahBravo(){
+    setBravo(bravo + 1)
+  }
+
+  function handlerGantiNama(){
+    setNama('Expresidentz')
+  }
+
   return (
-    <div>
+    <div className='body'>
       <div className="banner-container">
         {/* kartunya */}
         <div className="header-banner-wrapper">
@@ -13,27 +31,37 @@ export default function Home() {
             <Image
                 src="/asset/profil.png"
                 alt="Picture of the author"
-                width={500}
-                height={500}
+                fill
+                objectFit='contain'
               />
-
           </div>
           <div className="content-header-banner">
             {/* nama dan kawan kawan */}
-            <h1>Siti Nurhaliza</h1>
+            <h1>{nama}</h1>
             <div className="bio-nim-header-banner">
               {/* nim-bio */}
               <p>D121211028</p>
-              <p>Semangat Yok</p>
+              <p>Semangat {bravo} Yok</p>
             </div>
           </div>
         </div>
         <div className="cta-banner-wrap">
           {/* tombol cta */}
-        </div>
+          <div className='cta-button' onClick={handlerTambahBravo}>
+              <p>Halo!</p>
+            </div>
+            <div className='cta-button' 
+            style={{
+              marginTop: '12px'
+            }}
+            
+            onClick={handlerGantiNama}>
+              <p>Ganti Nama</p>
+            </div>
+        </div>         
       </div>
-
     </div>
+
   )
 }
       
