82fe6b5f539c0ff267bc9f6a2da89e0157c0017a
NurhalizaSulthan <nurhalizas21d@student.unhas.ac.id>
2023-11-23 15:19:01 +0800

    feat: ndk muncul

--- src/app/diary/page.js
+++ src/app/diary/page.js
@@ -2,12 +2,10 @@
 import "@style/diary.css";
 import { useEffect, useState } from "react";
 import axios from "axios";
-import Link from "next/link";
 
 export default function Diary() {
   const [judul, setJudul] = useState([]);
   const [isiDiary, setIsiDiary] = useState([]);
-  const[isData, setData] = useState([]);
 
   const endpointAPI = "https://6555c3f284b36e3a431e4764.mockapi.io/diaryku";
 
@@ -33,14 +31,13 @@ export default function Diary() {
       {judul.length > 0 ? (
         <ul>
           {judul.map((item, idx) => (
-            <Link href={`/diary/${item}/${isiDiary[idx]}`} style={{ textDecoration: 'none' }}>
-              <li key={idx}>
+              <li>
                 <div className="diary-container">
-                  <h1>{judul[idx]}</h1>
-                  <p className="p-diary">{isiDiary[idx]}</p>
+                    <h1>{judul[idx]}</h1>
+                    <p className="p-diary">{isiDiary[idx]}</p>
                 </div>
               </li>
-            </Link>
+
           ))}
         </ul>
       ) : (
