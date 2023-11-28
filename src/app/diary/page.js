d10061bb2e7951d2934db42c98137aeead24ec39
NurhalizaSulthan <nurhalizas21d@student.unhas.ac.id>
2023-11-28 12:59:44 +0800

    feat: perbaiki

--- src/app/diary/page.js
+++ src/app/diary/page.js
@@ -2,24 +2,34 @@
 import "@style/diary.css";
 import { useEffect, useState } from "react";
 import axios from "axios";
+import Link from "next/link";
 
 export default function Diary() {
   const [judul, setJudul] = useState([]);
   const [isiDiary, setIsiDiary] = useState([]);
+  const [isData, setData] = useState([]);
 
   const endpointAPI = "https://6555c3f284b36e3a431e4764.mockapi.io/diaryku";
 
   async function getDiary() {
-    const res = await axios.get(endpointAPI);
-    const data = res.data;
+    try {
+      const res = await axios.get(endpointAPI);
 
-    //ambil judul
-    const judul = data.map((item) => item.judul);
-    setJudul(judul);
+      //ambil data
+      const dataJSON = res.data;
+      console.log("dalam", dataJSON);
+      setData(dataJSON);
 
-    //ambil isi_diary
-    const isi_diary = data.map((item) => item.isi_diary);
-    setIsiDiary(isi_diary);
+      //ambil judul
+      const judul = dataJSON.map((item) => item.judul);
+      setJudul(judul);
+
+      //ambil isi_diary
+      const isi_diary = dataJSON.map((item) => item.isi_diary);
+      setIsiDiary(isi_diary);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
   }
 
   useEffect(() => {
@@ -27,17 +37,22 @@ export default function Diary() {
   }, []);
 
   return (
+    
     <div className="diary-list-container">
       {judul.length > 0 ? (
         <ul>
           {judul.map((item, idx) => (
-              <li>
+            <Link
+              href={`/diary/${item}/${isiDiary[idx]}`}
+              style={{ textDecoration: "none" }}
+            >
+              <li key={idx}>
                 <div className="diary-container">
-                    <h1>{judul[idx]}</h1>
-                    <p className="p-diary">{isiDiary[idx]}</p>
+                  <h1>{judul[idx]}</h1>
+                  <p className="p-diary">{isiDiary[idx]}</p>
                 </div>
               </li>
-
+            </Link>
           ))}
         </ul>
       ) : (
